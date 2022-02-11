cd89360566943b99c5dc5012bb5ee6a05bd58187
H0PP3R <edenjia@hotmail.com>
2022-02-11 22:35:38 +0000

    decided to commit viewPane changes to separate branch from main, modified TablePane to accept parsed data and to show data as table, in the middle of scrollbar implementation

--- src/components/tablePane.py
+++ src/components/tablePane.py
@@ -1,11 +1,35 @@
 from tkinter import *
 from  tkinter import ttk
+from styling import TABLE_STYLING
+class TablePane(ttk.Frame):
+  def __init__(self, parent, data, mode='read'):
+    self.data = data
+    self.frame = ttk.Frame(parent)
+    self.frame.pack(fill="x")
 
-# Not fully functional, have to clean data and design
-class tablePane(ttk.Frame):
-  def __init__(self, parent, data):
-    for i in range(len(data)):
-      for j in range(len(data[0])):
-        self.e = Entry(parent, width=20)
-        self.e.grid(row=i, column=j)
-        self.e.insert(END, data[i][j])
+    if mode=='read':
+      self._populateViewTable()
+    elif mode=='write':
+      # for edit version -----
+      # for i in range(len(data)):
+      #   for j in range(len(data[0])):
+      #     self.e = Entry(self.frame, width=20)
+      #     self.e.grid(row=i, column=j)
+      #     self.e.insert(END, data[i][j])
+      pass
+    else:
+      pass
+
+  def _populateViewTable(self):
+    self.headings = self.data[0]
+    self.table = ttk.Treeview(self.frame, columns=self.headings, show='headings')
+    self.table.pack(fill="x")
+    self.table['columns'] = tuple(self.headings)
+
+    for i in range(len(self.headings)):
+      self.table.column(self.headings[i], anchor=TABLE_STYLING["ANCHOR"], width=80)
+    for i in range(len(self.headings)):
+      self.table.heading(self.headings[i], text=str(self.headings[i]), anchor=TABLE_STYLING["ANCHOR"])
+    for i in range(1, len(self.data)):
+        self.table.insert(parent='',index='end',iid=i, text='',
+          values=(tuple(self.data[i])))
