03ec5e9a090bdc51f1091f1ac23b174079792139
H0PP3R <edenjia@hotmail.com>
2022-03-02 17:10:22 +0000

    initial commit

--- src/app.py
+++ src/app.py
@@ -1,13 +1,30 @@
-from tkinter import Tk
-from components.collapsiblePane import ToggledFrame as cp
+from tkinter import Tk, Button
 from styling import *
 from viewPane import ViewPane
+from editPane import EditPane
 
 class App():
   def __init__(self):
-    root = Tk()
-    root.geometry(f"{ROOT_SIZE[0]}x{ROOT_SIZE[1]}")
+    self.mainPane = None
+    self.root = Tk()
+    self.root.geometry(f"{ROOT_SIZE[0]}x{ROOT_SIZE[1]}")
 
-    self.viewPane = ViewPane(root)
+    self.viewPane = ViewPane(self.root)
+    self.mainPane = self.viewPane
+    self.paneID = 0
+    button = Button(self.root, text ="switch", command=self.toggleView)
+    button.pack(side='left')
 
-    root.mainloop()
+    self.root.mainloop()
+  
+  def toggleView(self):
+    if self.paneID == 0: 
+      del self.viewPane
+      self.editPane = EditPane(self.root)
+      self.mainPane = self.editPane
+      self.paneID = 1
+    elif self.paneID == 1:
+      del self.editPane
+      self.viewPane = ViewPane(self.root)
+      self.mainPane = self.viewPane
+      self.paneID = 0  
